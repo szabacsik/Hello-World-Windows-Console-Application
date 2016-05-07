8334cd456c325a96bfb2267237d850ed793145d4
András Szabácsik <packman@freemail.hu>
2016-05-07 11:35:36 +0200

    Basic

---
+++ Hello-World/Hello-World/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello hello = new Hello();
+        }
+    }
+
+    public class Hello
+    {
+        static Hello()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
